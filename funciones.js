6dd7b580ec22f4bf1e826123ce65a48e59df11cd
JEdwinM <jedwinmonsalve@gmail.com>
2026-02-20 20:54:34 -0500

    Creacion del core del calculo IMC

--- funciones.js
+++ funciones.js
@@ -1,3 +1,28 @@
-console.log("esta conectado el js");
+let peso = 80;
+let altura = 1.75;
+let resultadoImc = 0;
+let caracteristica = "";
+let mensaje = "";
 
-alert("hola soy un mensaje");
+//calculo IMC = peso / (altura**2)
+
+resultadoImc = peso / (altura**2);
+
+
+// imc < 18.5 : bajo peso
+// imc 18.5 y 25 : peso normal
+// imc 25 y 30 : sobrepeso
+// imc > 30 : obeso
+
+
+if(resultadoImc < 18.5){
+    caracteristica="BAJO PESO";
+}else if (resultadoImc < 25) {
+     caracteristica="NORMAL";
+}else if (resultadoImc < 30) {
+     caracteristica="SOBREPESO";
+}else{
+     caracteristica = "OBESIDAD";
+}
+
+console.log("su caracteristica es: "+caracteristica + " su resultado IMC:"+resultadoImc);
